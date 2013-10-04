5fda77ccf03139957079781a3df4b3d9212b88a1
Alisher Ulugbekov <ulugbekov@Alishers-MacBook-Pro.local>
2013-10-04 12:35:33 -0600

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Veterinary::Application.config.secret_token = 'ff81f2b2dbaeef0da3540f43a62ba645f6499da5a509146b186e57bfb82f434a88f7425a9da0ee096473c2b057bf272b008f8e0dfb9963d300a09c0b40728dbc'
